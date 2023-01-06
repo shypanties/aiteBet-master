aa2817c272f9bb60e7b655016b99084d3443c66b
shypanties <112874692+shypanties@users.noreply.github.com>
2023-01-06 21:43:12 +0800

    Delete TODO.js

--- TODO.js
+++
@@ -1,8 +0,0 @@
-// state management (refactor)
-// optimistic loading on adding new scores
-// animations - css? library?
-// one get call or multiple based on type?
-// set up auth0
-// redesign
-// find appropriate UX good practices
-// check context caveat to cause unneeded rerenders
